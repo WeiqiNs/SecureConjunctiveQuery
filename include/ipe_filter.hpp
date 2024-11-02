0d5b1e744fd1be4aa8f9149760e32ca6fe91a7ef
Weiqi97 <weltch1997@gmail.com>
2024-11-02 15:55:12 -0400

    docstrings and update README.md

--- include/ipe_filter.hpp
+++ include/ipe_filter.hpp
@@ -19,13 +19,45 @@ struct IpeFilterMsk{
 
 class IpeFilter{
 public:
+    /**
+     * Generate the required public parameters.
+     * @param degree the degree of the polynomial.
+     * @param length the length of vectors to the encryption algorithm.
+     * @param pre whether we want to use the precomputed table for group exponentiation.
+     * @return the generated public parameters.
+     */
     static IpeFilterPP pp_gen(int degree, int length, bool pre = true);
 
+    /**
+     * Generate master secret key.
+     * @param pp the public parameters.
+     * @return the generated master secret key.
+     */
     static IpeFilterMsk msk_gen(const IpeFilterPP& pp);
 
+    /**
+     * Perform the Filter FE encryption.
+     * @param pp the public parameters.
+     * @param msk the master secret key.
+     * @param x a vector of strings or integers.
+     * @return the ciphertext.
+     */
     static G1Vec enc(const IpeFilterPP& pp, const IpeFilterMsk& msk, const Vec& x);
 
+    /**
+     * Perform the Filter FE key generation.
+     * @param pp the public parameters.
+     * @param msk the master secret key.
+     * @param y a vector or matrix of strings or integers. Use matrix only when you are selecting set of inputs.
+     * @return the function key.
+     */
     static G2Vec keygen(const IpeFilterPP& pp, const IpeFilterMsk& msk, const Mat& y);
 
+    /**
+     * Perform the Filter FE decryption.
+     * @param ct the ciphertext.
+     * @param sk the function key.
+     * @return a boolean indicating the result of Filter.
+     */
     static bool dec(const G1Vec& ct, const G2Vec& sk);
 };
--- include/ipe_join.hpp
+++ include/ipe_join.hpp
@@ -22,13 +22,46 @@ struct IpeJoinMsk{
 
 class IpeJoin{
 public:
+    /**
+     * Generate the required public parameters.
+     * @param degree the degree of the polynomial.
+     * @param length the length of vectors to the encryption algorithm.
+     * @param pre whether we want to use the precomputed table for group exponentiation.
+     * @return the generated public parameters.
+     */
     static IpeJoinPP pp_gen(int degree, int length, bool pre = true);
 
+    /**
+     * Generate master secret key.
+     * @param pp the public parameters.
+     * @return the generated master secret key.
+     */
     static IpeJoinMsk msk_gen(const IpeJoinPP& pp);
 
+    /**
+     * Perform the Equal-Join FE encryption.
+     * @param pp the public parameters.
+     * @param msk the master secret key.
+     * @param x a vector of strings or integers.
+     * @param join_index indicate which position to check for equality.
+     * @return the ciphertext.
+     */
     static G1Vec enc(const IpeJoinPP& pp, const IpeJoinMsk& msk, const Vec& x, int join_index);
 
+    /**
+     * Perform the Equal-Join FE key generation.
+     * @param pp the public parameters.
+     * @param msk the master secret key.
+     * @param y a matrix of strings or integers.
+     * @return the function key.
+     */
     static G2Vec keygen(const IpeJoinPP& pp, const IpeJoinMsk& msk, const Mat& y);
 
+    /**
+     * Perform the Equal-Join decryption.
+     * @param ct the ciphertext.
+     * @param sk the function key.
+     * @return a Gt element of the pairing, needs to be compared with another to determine whether join can happen.
+     */
     static Gt dec(const G1Vec& ct, const G2Vec& sk);
 };
